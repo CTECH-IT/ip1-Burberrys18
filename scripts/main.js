ec884073082d0f1c5a4edfa48909e40a0c11aa7f
Burberrys18 <98343136+Burberrys18@users.noreply.github.com>
2022-02-07 11:34:27 -0600

    save

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let x = canvas.width - 100;
+let ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc
+}
