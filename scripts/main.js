8612248ecef2cbeb33b85ba00a353978598d0a87
Imran Senyonjo <1019654@isd535.org>
2022-02-08 08:58:30 -0600

    trying random

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+let y = (canvas.height/2) - 200
 let x = canvas.width - 100;
 let ballRadius = 10;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc
+    ctx.arc(x, y, ballRadius, 0, math.PI*2);
+    ctx.fillStyle =  "lime";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    drawBall();
+    setInterval(2)
 }
