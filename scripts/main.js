d96d63740ce0c624fc7f5b16cbf71102eeae1a6f
Imran Senyonjo <1019654@isd535.org>
2022-02-09 10:24:12 -0600

    trying to figure out the whole mess

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,11 @@ let x = canvas.width - 100;
 
 let ballRadius = 10;
 
+let mouseClicked = false;
+
 let score = 0;
+
+let timer = 70;
 
 function drawBall() {
     ctx.beginPath();
@@ -26,18 +30,46 @@ function draw() {
     x = Math.random() * canvas.width;
     y = Math.random() * canvas.height;
 
+    do {
+        setInterval(interval);
+    } while (timer);
+
     drawScore();
+
+    drawTimer();
 }
 
-let interval = setInterval(draw, 1000);
+//setting different intervals//
+let interval = setInterval(draw, 1200);
+let int = setInterval(draw, 1000);
+let er = setInterval(draw, 700);
+let val = setInterval(draw, 500);
 
 function drawScore() {
-    ctx.font = "20px Arial";
+    ctx.font = "18px Arial";
     ctx.fillStyle = "blue"; 
-    ctx.fillText("Score: " + score, 8, 20);
+    ctx.fillText("Score: " + score, 280, 20);
 }
 
-//when the ball is clicked, the score increases by 1//
-function onclick(drawBall) {
-    score++;
+function drawTimer() {
+    ctx.font = "18px Arial";
+    ctx.fillStyle = "lime";
+    ctx.fillText("Timer: " + timer, 8, 20);
+}
+
+document.addEventListener("mousedown", mouseDownHandler, false);
+document.addEventListener("mouseup", mouseUpHandler, false);
+
+function mouseDownHandler(e) {
+    if (e.button==1 || e.button==2) {
+        mouseClicked = true;
+        //if mouseClicked is true, add on the score by 1//
+        score++;
+    }
+}
+
+function mouseUpHandler(e) {
+    if (e.button==1 || e.button==2) {
+        mouseClicked = false;
+    }
 }
